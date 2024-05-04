de5ff1c01ab20e6a0b2ab1c9d016648b085362fb
Yinkaxlusive <141252881+Yinkaxlusive@users.noreply.github.com>
2024-05-04 01:02:15 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,40 +1,12 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+# sets up the web servers for the deployment of web_static
 
-sudo apt-get update
-sudo apt-get install -y nginx
-
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-echo "Hello World" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-sudo chown -R ubuntu:ubuntu /data/
-
-# Update Nginx configuration
-config="server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By \$HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-   
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-   
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-   
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}"
-
-echo "$config" | sudo tee /etc/nginx/sites-available/default > /dev/null
-
-sudo service nginx restart
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
