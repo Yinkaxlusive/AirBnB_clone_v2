6bc237de5ab027ff7ae24878c53c4c8119a102f7
Yinkaxlusive <141252881+Yinkaxlusive@users.noreply.github.com>
2024-05-05 02:18:20 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,41 +1,27 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
-
+# Bash script to set up web servers for the deployment of web_static
 sudo apt-get update
-sudo apt-get install -y nginx
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
-echo "Hello World" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx configuration
-config="server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By \$HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-   
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-   
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-   
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}"
-
-echo "$config" | sudo tee /etc/nginx/sites-available/default > /dev/null
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
-
