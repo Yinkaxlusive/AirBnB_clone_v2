c1b995b8e82946deaac394e91926472cfc58ed7f
Yinkaxlusive <141252881+Yinkaxlusive@users.noreply.github.com>
2024-05-20 01:01:33 +0100

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states_list: HTML page with a list of all State objects in DBStorage.
 """
-Flask web application to display a list of states
-"""
-from flask import Flask, render_template
 from models import storage
-from models.state import State
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/states_list', strict_slashes=False)
+
+@app.route("/states_list", strict_slashes=False)
 def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage.
+
+    States are sorted by name.
     """
-    Route to display a list of states
-    """
-    states = storage.all(State)
-    sorted_states = sorted(states.values(), key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """
-    Method to remove the current SQLAlchemy Session
-    """
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
